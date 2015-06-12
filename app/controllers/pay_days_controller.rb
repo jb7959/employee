e1a55427e6e9b1675e56d200e8f435b602ff8963
JerryAhn <jb7959@naver.com>
2015-06-12 07:02:39 +0000

    added the  clean_pay_day for salaryman pay_day module

--- app/controllers/pay_days_controller.rb
+++ app/controllers/pay_days_controller.rb
@@ -84,8 +84,8 @@ class PayDaysController < ApplicationController
     
       @calculate_pays.each do |payment|
         if @salaryman.where(emp_id: payment.emp_id).exists?  # @salaryman.where(emp_id: payment.emp_id).exists?는 해당 요소가 존재하는지에 대한 불린값을 제공
-          if Time.new == getDayOfWeekForLastDay #월말평일일 경우 동작
-             PayDay.create(emp_id: payment.emp_id, pay: payment.pay)
+          if Time.new == getDayOfWeekForLastDay #월말평일일 경우 동작 #월급쟁이는 매달 마지막 평일에 급여를 받음
+             PayDay.create(emp_id: payment.emp_id, pay: payment.pay, lastPayday: Time.new)
           end
           
         elsif @hourlyman.where(emp_id: payment.emp_id).exists?  # @salaryman.where(emp_id: payment.emp_id).exists?는 해당 요소가 존재하는지에 대한 불린값을 제공
@@ -103,9 +103,13 @@ class PayDaysController < ApplicationController
     
     end
     
-    def clean_pay_day #중복되는 레코드 삭제
+    def clean_pay_day #중복되는 지난 레코드 삭제
       @pay_days = PayDay.all
-      @pay_days.each do |pays| pays.destroy() end
+      @pay_days.each do |pays| 
+        if pays.lastPayday <= Time.now
+          pays.destroy()
+        end
+      end
     end
     
     def getDayOfWeekForLastDay(targetYear=Time.new.year, targetMonth=Time.new.month) #월말 평일 확인용 함수 #인자로 변수를 선언함으로 써, 클로저Clouser 기능 사용
